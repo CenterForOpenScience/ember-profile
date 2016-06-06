9983b5ebae55e3b7fc9fc1d1c0b0aeff44b3fbb4
Luke Marsh <mluke93@vt.edu>
2016-06-06 16:55:23 -0400

    Personal add

--- app/routes/profile/about.js
+++ app/routes/profile/about.js
@@ -4,9 +4,9 @@ export default Ember.Route.extend({
   model() {
     return {
       personal: {
-        bio: 'Help me',
-        phone: '1234',
-        join_date: 'tomorrow'
+        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        phone: '(123) 456-7890',
+        join_date: '5-16-2016'
       },
       social: [{
         name: 'Twitter',
