e07a74e3044647f90d8a363dab1bf28f0d0184e0
Ellis Tsung <el1t@ymail.com>
2016-06-06 15:21:14 -0400

    Mock about

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      name: 'John Doe',
+      location: 'Charlottesville, VA',
+      profession: 'Developer'
+    };
+  }
 });
--- app/routes/profile/about.js
+++ app/routes/profile/about.js
@@ -1,4 +1,37 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      personal: {
+        bio: 'Help me',
+        phone: '1234',
+        join_date: 'tomorrow'
+      },
+      social: [{
+        name: 'Twitter',
+        link: '/'
+      }, {
+        name: 'LinkedIn',
+        link: '/'
+      }],
+      employment: [{
+        name: 'Center for Closed Science',
+        department: 'internets',
+        title: 'pro',
+        dates: 'yesterday - today'
+      }, {
+        name: 'Center for Open Science',
+        department: 'Infrastructure',
+        title: 'Developer',
+        dates: 'tomorrow - saturday'
+      }],
+      education: [{
+        name: 'Ellis\' Ember Education Experiment',
+        department: 'Learning',
+        degree: '90º',
+        dates: '6/6/16 - 6 June 2016'
+      }]
+    };
+  }
 });
