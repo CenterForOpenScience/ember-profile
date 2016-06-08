b42ead0100dbfa01d684d2b5f520db2b661c9109
Baylee Swenson <baylee.d.swenson@vcsu.edu>
2016-06-08 09:41:46 -0400

    Start of editable fields

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -7,5 +7,27 @@ export default Ember.Route.extend({
       location: 'Charlottesville, VA',
       profession: 'Developer'
     };
+  },
+  actions: {
+  	toggle: function(){
+		$('.inactive-edit, .active-edit').toggle();
+		$('main').fadeTo('fast',0.3);
+		$('.profile-content span').each(function(){ 
+			if($(this).find('input').length){
+                $(this).text($(this).find('input').val());
+            } else if($(this).find('textarea').length){
+            	$(this).text($(this).find('textarea').val());
+            } else {
+                var t = $(this).text();
+                if($(this).attr('id') == 'bio-text'){
+                	$(this).text('').append($('<textarea class="form-control" />',{'value' : t}).val(t));
+                }else{
+                	 $(this).text('').append($('<input class="form-control" />',{'value' : t}).val(t));
+
+                }
+            }
+        });
+		$('main').fadeTo('fast',1.0);
+  	}
   }
 });
