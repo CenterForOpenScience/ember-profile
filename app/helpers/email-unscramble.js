a83aa52d37925cea46b590f9d38d19b6207df355
Ellis Tsung <el1t@ymail.com>
2016-06-16 14:48:48 -0400

    Small fixes

--- app/helpers/email-unscramble.js
+++ app/helpers/email-unscramble.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export function emailUnscramble([email, date]/*, hash*/) {
   let decoded = '';
   // XOR
-  for (let i = 0; i < Math.max(email.length, date.length); i++) {
-    decoded += String.fromCharCode(email.charCodeAt(i % email.length) ^ date.charCodeAt(i % date.length));
+  for (let i = 0; i < email.length; i++) {
+    decoded += String.fromCharCode(email.charCodeAt(i) ^ date.charCodeAt(i % date.length));
   }
   return decoded;
 }
