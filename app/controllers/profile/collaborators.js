d7a8c2f139d4dabc8ce8fed42ba8e6f0759dab00
Ellis Tsung <el1t@ymail.com>
2016-06-27 16:44:07 -0400

    Sort collaborators

--- app/controllers/profile/collaborators.js
+++ app/controllers/profile/collaborators.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   sort: ['projects:desc'],
   actions: {
-    filter(){
-      var filter = Ember.$('#collab-filters option:selected').val();
+    filter() {
+      const filter = Ember.$('#collab-filters option:selected').val();
       this.set('sort', [filter]);
     }
   }
