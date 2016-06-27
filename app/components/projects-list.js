b811825346686f9663fddda5592b9b3c3c33c6ee
Ellis Tsung <el1t@ymail.com>
2016-06-27 16:43:31 -0400

    Pull project collaborators from API

--- app/components/projects-list.js
+++ app/components/projects-list.js
@@ -6,10 +6,17 @@ export default CpPanelsComponent.extend({
   elementId: 'projects-list',
   accordion: true,
   sorted: Ember.computed.sort('model', 'sortDefinition'),
-  sortDefinition: ['name:asc'],
-  actions: {
-    select(project) {
-      this.get('onSelect')(project);
-    }
+  store: Ember.inject.service(),
+  projectCollaborators: {},
+  didInsertElement() {
+    this.get('store').find('user', 'd94fu').then(me =>
+      this.get('model').forEach(node => {
+        this.get('projectCollaborators')[node] = [];
+        node.get('contributors').then(contributors =>
+          contributors.forEach(contributor =>
+            contributor.get('users').then(user =>
+              me === user ? '' : this.get('projectCollaborators')[node].pushObject(user.get('fullName'))
+            )));
+      }));
   }
 });
--- app/helpers/project-collaborators.js
+++ app/helpers/project-collaborators.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
-export function projectCollaborators([collaborators]/*, hash*/) {
-  return collaborators ? collaborators.join('; ') : 'None';
+export function projectCollaborators([project, map]/*, hash*/) {
+  return map[project] ? map[project].join('; ') : 'None';
 }
 
 export default Ember.Helper.helper(projectCollaborators);
